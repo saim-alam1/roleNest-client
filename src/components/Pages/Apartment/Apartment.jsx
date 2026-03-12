75ed0767cf4c27daf6b3faddac02a4d60837a8a2
saim-alam1 <allbehassansaim@gmail.com>
2026-03-12 21:02:10 +0600

    Added Range Search Feature In Apartments Component

--- src/components/Pages/Apartment/Apartment.jsx
+++ src/components/Pages/Apartment/Apartment.jsx
@@ -3,12 +3,16 @@ import Loading from "../Loadings/Loading";
 import Card from "../Shared/Card/Card";
 import useAxios from "../../../Hooks/useAxios";
 import { useState } from "react";
+import Lottie from "lottie-react";
+import noDataFound from "../../../assets/noDataFound.json";
 
 const Apartment = () => {
   const axiosInstance = useAxios();
   const [alreadyRequested, setAlreadyRequested] = useState(false);
   const [currentPage, setCurrentPage] = useState(0);
   const itemsPerPage = 6;
+  const [minRent, setMinRent] = useState("");
+  const [maxRent, setMaxRent] = useState("");
 
   // Apartment Count Number
   const { data: countData = {} } = useQuery({
@@ -41,10 +45,14 @@ const Apartment = () => {
   if (isLoading) return <Loading />;
   if (error) return "An error has occurred: " + error.message;
 
-  const handleRentRange = (e) => {
-    const userRange = parseInt(e.target.value);
-    console.log(userRange);
-  };
+  // The Search Feature
+  const filteredApartments = apartmentsData.filter((apartment) => {
+    const rent = apartment?.rent;
+    if (minRent && rent < parseInt(minRent)) return false;
+    if (maxRent && rent > parseInt(maxRent)) return false;
+
+    return true;
+  });
 
   return (
     <section className="my-28 px-3">
@@ -58,20 +66,42 @@ const Apartment = () => {
           exclusive discounts on you dream apartments.
         </p>
       </div>
+
+      {/* Range */}
       <div className="my-14 text-heading text-center max-w-9/12 mx-auto">
         <h4 className="text-[16px] font-bold text-heading mb-4">
           Search Your Rent Range
         </h4>
-        <input
-          type="number"
-          onChange={handleRentRange}
-          placeholder="Rent Range"
-          className="block  mt-2 w-full placeholder-gray-400/70 dark:placeholder-gray-500 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
-        />
+        <div className="flex items-center gap-1.5 justify-end">
+          <input
+            type="number"
+            value={minRent}
+            onChange={(e) => setMinRent(e.target.value)}
+            placeholder="Minimum"
+            className="block  mt-2 w-full placeholder-gray-400/70 dark:placeholder-gray-500 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
+          />
+          <span>-</span>
+          <input
+            type="number"
+            value={maxRent}
+            onChange={(e) => setMaxRent(e.target.value)}
+            placeholder="Maximum"
+            className="block  mt-2 w-full placeholder-gray-400/70 dark:placeholder-gray-500 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
+          />
+        </div>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {apartmentsData.map((apartmentsCard) => (
+        {filteredApartments.length === 0 && (minRent || maxRent) && (
+          <div className="col-span-full flex flex-col items-center justify-center flex-1 my-8 space-y-4">
+            <Lottie animationData={noDataFound} className="w-full max-w-md" />
+            <p className="text-center col-span-3  font-semibold">
+              ⚠️ No apartments found here.
+            </p>
+          </div>
+        )}
+
+        {filteredApartments.map((apartmentsCard) => (
           <Card
             key={apartmentsCard._id}
             apartmentsCard={apartmentsCard}
