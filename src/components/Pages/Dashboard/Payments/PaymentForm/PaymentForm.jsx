470c07af7535473533dbbb24c0734921bc4a9a54
saim-alam1 <allbehassansaim@gmail.com>
2026-03-29 20:18:25 +0600

    Card Error message and Test card

--- src/components/Pages/Dashboard/Payments/PaymentForm/PaymentForm.jsx
+++ src/components/Pages/Dashboard/Payments/PaymentForm/PaymentForm.jsx
@@ -1,4 +1,4 @@
-import { use } from "react";
+import { use, useState } from "react";
 import { AuthContext } from "../../../../../Contexts/AuthContext";
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import useAxios from "../../../../../Hooks/useAxios";
@@ -8,8 +8,9 @@ const PaymentForm = () => {
   const stripe = useStripe();
   const elements = useElements();
   const axiosInstance = useAxios();
+  const [error, setError] = useState("");
 
-  const handlePay = (e) => {
+  const handlePay = async (e) => {
     e.preventDefault();
 
     if (!stripe || !elements) return;
@@ -17,15 +18,28 @@ const PaymentForm = () => {
     const card = elements.getElement(CardElement);
 
     if (!card) return;
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      setError(error.message);
+    } else {
+      setError("");
+      console.log("paymentMethod", paymentMethod);
+    }
   };
 
   return (
-    <form onSubmit={handlePay} className="mt-8 flex items-center gap-3">
+    <form onSubmit={handlePay} className="mt-8gap-3">
       <CardElement className="p-4 border border-gray-300 rounded-lg w-full" />
 
-      <button type="submit" disabled={!stripe} className="btn btn-primary">
+      <button type="submit" disabled={!stripe} className="btn btn-primary my-4">
         Pay Now
       </button>
+      {error && <p className="text-red-500">{error}</p>}
     </form>
   );
 };
